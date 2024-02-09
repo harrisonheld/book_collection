bd0214f8dac07f90ca0e82cb0772c5ae751d1c03
Harrison Held <harrydheld@gmail.com>
2024-02-09 14:04:11 -0600

    Added user and user_book

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   resources :books do
     get 'delete', on: :member
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "user_books#index"
 end
