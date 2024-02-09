7d568617237d4bfa783ff56816b3056d85a7ebf4
Harrison Held <harrydheld@gmail.com>
2024-02-09 14:22:30 -0600

    Added tables and styling.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :user_books
   resources :users
-  resources :books do
-    get 'delete', on: :member
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "user_books#index"
+
+  resources :books do
+    member do
+      get 'delete'
+    end
+  end
 end
